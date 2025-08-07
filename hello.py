f0ab1e1ff87fe7f3dc7e1c6e2a176fd02f7072b1
Kriwich <dmdogfat@gmail.com>
2025-08-07 20:41:53 +0700

    feat: add current date to greeting

--- hello.py
+++ hello.py
@@ -1,7 +1,13 @@
 import config
+import datetime # เพิ่มบรรทัดนี้
 def say_hello(name):
     print(f"Hello, {name} from {config.APP_NAME}!")
-    
+
+def say_hello(name):
+    now = datetime.datetime.now() # เพิ่มบรรทัดนี้
+    print(f"Hello, {name} from {config.APP_NAME}!")
+    print(f"Today is {now.strftime('%Y-%m-%d')}.") # เพิ่มบรรทัดนี้    
+
 def greet_user():
     name = input("Please enter your name: ")
     say_hello(name)
